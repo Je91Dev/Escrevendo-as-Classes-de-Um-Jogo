1d0501aa319c00dae0bd082d2316e38df6e53d17
Jean <jean-oliveira-10@hotmail.com>
2024-02-25 08:37:31 -0300

    commit

---
+++ index.js
@@ -0,0 +1,40 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque = "";
+
+        switch (this.tipo) {
+            case "mago":
+                ataque = "magia";
+                break;
+
+            case "guerreiro":
+                ataque = "espada";
+                break;
+
+            case "monge":
+                ataque = "artes marciais";
+                break;
+
+            case "ninja":
+                ataque = "shuriken";
+                break;
+
+            default:
+                ataque = "ataque desconhecido";
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+const mago = new Heroi("Merlin", 35, "mago");
+const guerreiro = new Heroi("Arthur", 30, "guerreiro");
+
+mago.atacar();
+guerreiro.atacar(); 
